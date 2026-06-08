47d76483ed1f64154e41b4be56bcb9eeecd5dbfb
FingerFungus <204243820+FingerFungus@users.noreply.github.com>
2026-06-08 13:40:28 -0700

    erm

---
+++ Test1/Assets/Davin/Win.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinGame : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject winPanel;
+
+    void Start()
+    {
+    
+        winPanel.SetActive(false);
+    }
+
+    public void TriggerWin()
+    {
+        winPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
